afce0f8222ab8bedc6547d5df2696d6bbb7cddd0
Mario Jaun <mario.jaun@zuehlke.com>
2026-04-23 14:58:20 +0000

    add logging framework

--- app/src/main.c
+++ app/src/main.c
@@ -1,7 +1,13 @@
-#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
 
 int main(void)
 {
-    printk("Hello World! %s\n", CONFIG_BOARD_TARGET);
+    while (true) {
+        LOG_INF("Hello World! %s", CONFIG_BOARD_TARGET);
+        k_sleep(K_MSEC(1000));
+    }
+
     return 0;
 }
