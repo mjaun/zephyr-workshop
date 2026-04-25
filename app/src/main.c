80a0eed0880edbb371a1e670145927664d592dd0
Mario Jaun <mario.jaun@zuehlke.com>
2026-04-25 16:28:32 +0000

    add sensor and pwm output

--- app/src/main.c
+++ app/src/main.c
@@ -4,10 +4,6 @@ LOG_MODULE_REGISTER(main);
 
 int main(void)
 {
-    while (true) {
-        LOG_INF("%s %s", CONFIG_APP_HELLO_WORLD_MSG, CONFIG_BOARD_TARGET);
-        k_sleep(K_MSEC(1000));
-    }
-
+    LOG_INF("%s %s", CONFIG_APP_HELLO_WORLD_MSG, CONFIG_BOARD_TARGET);
     return 0;
 }
