5d3196c7418e7752b878049580bab13de9add15b
Mario Jaun <mario.jaun@zuehlke.com>
2026-04-27 12:08:20 +0000

    add application kconfig

--- app/src/main.c
+++ app/src/main.c
@@ -5,7 +5,7 @@ LOG_MODULE_REGISTER(main);
 int main(void)
 {
     while (true) {
-        LOG_INF("Hello World! %s", CONFIG_BOARD_TARGET);
+        LOG_INF("%s %s", CONFIG_APP_HELLO_WORLD_MSG, CONFIG_BOARD_TARGET);
         k_sleep(K_MSEC(1000));
     }
 
