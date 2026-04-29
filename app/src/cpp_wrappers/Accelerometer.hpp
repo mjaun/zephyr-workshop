2e998420dc8b591766da40a8083f14fe1a0723e5
Mario Jaun <mario.jaun@zuehlke.com>
2026-04-29 11:26:21 +0000

    cpp wrappers for drivers

---
+++ app/src/cpp_wrappers/Accelerometer.hpp
@@ -0,0 +1,30 @@
+#include "ApplicationAssert.hpp"
+#include <zephyr/drivers/sensor.h>
+
+class Accelerometer
+{
+public:
+    Accelerometer(const struct device *device) : _device(device) {}
+
+    float read()
+    {
+        struct sensor_value value = {};
+        int ret;
+
+        ret = sensor_sample_fetch(_device);
+        APP_ASSERT(ret == 0);
+
+        ret = sensor_channel_get(_device, SENSOR_CHAN_ACCEL_Z, &value);
+        APP_ASSERT(ret == 0);
+
+        return sensor_value_to_float(&value);
+    }
+
+    Accelerometer(const Accelerometer &) = delete;
+    Accelerometer &operator=(const Accelerometer &) = delete;
+    Accelerometer(Accelerometer &&) = delete;
+    Accelerometer &operator=(Accelerometer &&) = delete;
+
+private:
+    const struct device *_device;
+};
